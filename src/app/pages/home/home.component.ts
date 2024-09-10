8f05d0035915565734defef8d51becc96109cb7f
Gustavo Ariel <101509337+GustavoArielRos@users.noreply.github.com>
2024-09-10 00:32:48 -0300

    segundo commit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../../services/usuario.service';
+import { UsuarioListar } from '../../models/Usuario';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,25 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
+  //variavel que vai receber a lista de usuarios
+  usuarios: UsuarioListar[] = [];
+  usuariosGeral: UsuarioListar[] = [];
+
+  //fazendo a injeção de dependencia
+  //agora através do serviceUsuario eu consigo usar todos os metodos do usuarioService
+  constructor(private serviceUsuario:UsuarioService){}
+
+  //método que é roda quando se inicia o componente
+  ngOnInit(): void {
+    
+    //subcribe é sempre usado quando o método retorna um observable
+    //variavel response possui o resultado da requisição
+    this.serviceUsuario.GetUsuarios().subscribe(response => {
+        this.usuarios = response.dados;
+        this.usuariosGeral = response.dados;
+        console.log(response);
+    })
+  }
 }
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { UsuarioListar } from '../models/Usuario';
+import { Observable } from 'rxjs';
+import { Response } from '../models/Response';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +13,15 @@ export class UsuarioService {
   //usando a variável de ambiente(aquela que possui a URL da api do backend)
   ApiUrl = environment.UrlApi;
 
-  constructor() { }
+  //recebendo o httpcliet através da variável http
+  constructor(private http : HttpClient) { }
+
+  //método get usuários( para pegar o endpoint do get la da api e pegar as informações)
+  //Observable(espera a requisição dos dados antes de agir)
+  //esse método retorna um tipo de ResponseUsuarioListar
+  //esse Response e UsuarioListar são os tipos de objetos que eu criei na Model
+  GetUsuarios(): Observable<Response<UsuarioListar[]>>{
+    //indo no método get atráves da Url(ApiUrl) e pegando a lista
+    return this.http.get<Response<UsuarioListar[]>>(this.ApiUrl);
+  }
 }
